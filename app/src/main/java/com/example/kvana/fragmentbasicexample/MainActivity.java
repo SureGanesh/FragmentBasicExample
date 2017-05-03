bb1c9e601f1e7345821f0d36d4a00c9dc0c453b5
kvana <kvana@kvanas-iMac.local>
2017-05-02 18:00:07 -0700

    add toolbar and back button

--- app/src/main/java/com/example/kvana/fragmentbasicexample/MainActivity.java
+++ app/src/main/java/com/example/kvana/fragmentbasicexample/MainActivity.java
@@ -8,35 +8,49 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b_one,b_two;
+    Button btn_one,btn_two,btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b_one = (Button) findViewById(R.id.button_one);
-        b_two = (Button) findViewById(R.id.button_two);
+        btn_one = (Button) findViewById(R.id.button_one);
+        btn_two = (Button) findViewById(R.id.button_two);
+        btn_back= (Button) findViewById(R.id.btn_back);
 
-        b_one.setOnClickListener(new View.OnClickListener() {
+        btn_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
                 FragmentOne f1 = new FragmentOne();
                 ft.add(R.id.fragment_one,f1);
+                ft.addToBackStack(null);
                 ft.commit();
             }
         });
 
-
-        b_two.setOnClickListener(new View.OnClickListener() {
+        btn_two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
              FragmentManager FM = getFragmentManager();
                 FragmentTransaction FT =FM.beginTransaction();
                 FragmentTwo F2 = new FragmentTwo();
                 FT.add(R.id.fragment_two,F2);
+                FT.addToBackStack(null);
                 FT.commit();
+            }
+        });
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_back:
+                        onBackPressed();
+                        break;
+                }
+
             }
         });
     }
